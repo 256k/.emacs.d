d4b1cb92f122eb477119cb82c22b9349b0416c2e
256k <fayek.helmi@gmail.com>
2023-08-26 11:43:16 -0400

    core minimal basics config done

---
+++ early-init.el
@@ -0,0 +1,8 @@
+(setq package-enable-at-startup nil)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono for Powerline" :foundry "nil" :slant normal :weight regular :height 160 :width normal)))))
