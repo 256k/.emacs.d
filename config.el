b925ead4cb425b77d196044c94030085c8e22d04
256k <fayek.helmi@gmail.com>
2023-08-31 23:35:50 -0400

    added more packages

--- config.el
+++ config.el
@@ -1,3 +1,10 @@
+(setq backup-directory-alist '(("" . "~/.emacs.d/backup/")))
+(setq backup-by-copying t    ; Don't delink hardlinks
+    delete-old-versions t  ; Clean up the backups
+    version-control t      ; Use version numbers on backups,
+    kept-new-versions 5    ; keep some new versions
+    kept-old-versions 2)   ; and some old ones, too
+
 (defvar elpaca-installer-version 0.5)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
@@ -75,63 +82,83 @@
 ;; Don't install anything. Defer execution of BODY
 ;; (elpaca nil (message "deferred"))
 
-(use-package general
-  :config
-  (general-evil-setup)
+;; (use-package general
+;;   :config
+;;   (general-evil-setup)
 
-  ;; set up 'SPC' as the global leader key
-  (general-create-definer 256k/leader-keys
-    :states '(normal insert visual emacs)
-    :keymaps 'override
-    :prefix "SPC" ;; set leader
-    :global-prefix "M-SPC") ;; access leader in insert mode
+;;   ;; set up 'SPC' as the global leader key
+;;   (general-create-definer 256k/leader-keys
+;;     :states '(normal insert visual emacs)
+;;     :keymaps 'override
+;;     :prefix "SPC" ;; set leader
+;;     :global-prefix "M-SPC") ;; access leader in insert mode
 
-  (256k/leader-keys
-    "." '(find-file :wk "Find file")
-    "TAB TAB" '(comment-line :wk "Comment lines"))
+;;   (256k/leader-keys
+;;     "." '(find-file :wk "Find file")
+;;     "TAB TAB" '(comment-line :wk "Comment lines"))
 
-  (256k/leader-keys
-    "b" '(:ignore t :wk "buffer")
-    "b b" '(switch-to-buffer :wk "Switch buffer")
-    "b i" '(ibuffer :wk "Ibuffer")
-    "b k" '(kill-this-buffer :wk "Kill this buffer")
-    "b n" '(next-buffer :wk "Next buffer")
-    "b p" '(previous-buffer :wk "Previous buffer")
-    "b r" '(revert-buffer :wk "Reload buffer"))
+;;   (256k/leader-keys
+;;     "b" '(:ignore t :wk "buffer")
+;;     "b b" '(switch-to-buffer :wk "Switch buffer")
+;;     "b i" '(ibuffer :wk "Ibuffer")
+;;     "b k" '(kill-this-buffer :wk "Kill this buffer")
+;;     "b n" '(next-buffer :wk "Next buffer")
+;;     "b p" '(previous-buffer :wk "Previous buffer")
+;;     "b r" '(revert-buffer :wk "Reload buffer"))
 
-  (256k/leader-keys
-    "f"   '(:ignore t :wk "terminal")
-    "f f" '((lambda () (interactive) (find-file "./")) :wk "open file browser")
-    "f c" '((lambda () (interactive) (find-file "~/.emacs.d/config.org")) :wk "Edit emacs config")
-    "f r"   '(counsel-recentf :wk "toggle file details")
-    "d"   '(dired-hide-details-mode :wk "toggle file details"))
+;;   (256k/leader-keys
+;;     "f"   '(:ignore t :wk "terminal")
+;;     "f f" '((lambda () (interactive) (find-file "./")) :wk "open file browser")
+;;     "f c" '((lambda () (interactive) (find-file "~/.emacs.d/config.org")) :wk "Edit emacs config")
+;;     "f r"   '(counsel-recentf :wk "toggle file details")
+;;     "d"   '(dired-hide-details-mode :wk "toggle file details"))
 
-  (256k/leader-keys
-    "e" '(:ignore t :wk "Evaluate")    
-    "e b" '(eval-buffer :wk "Evaluate elisp in buffer")
-    "e d" '(eval-defun :wk "Evaluate defun containing or after point")
-    "e e" '(eval-expression :wk "Evaluate and elisp expression")
-    "e l" '(eval-last-sexp :wk "Evaluate elisp expression before point")
-    "e r" '(eval-region :wk "Evaluate elisp in region")) 
+;;   (256k/leader-keys
+;;     "e" '(:ignore t :wk "Evaluate")    
+;;     "e b" '(eval-buffer :wk "Evaluate elisp in buffer")
+;;     "e d" '(eval-defun :wk "Evaluate defun containing or after point")
+;;     "e e" '(eval-expression :wk "Evaluate and elisp expression")
+;;     "e l" '(eval-last-sexp :wk "Evaluate elisp expression before point")
+;;     "e r" '(eval-region :wk "Evaluate elisp in region")) 
 
-   (256k/leader-keys
-    "h" '(:ignore t :wk "Help")
-    "h f" '(describe-function :wk "Describe function")
-    "h v" '(describe-variable :wk "Describe variable")
-    ;; "h r r" '((lambda () (interactive) (load-file "~/.config/emacs/init.el")) :wk "Reload emacs config"))
-    "h r r" '(reload-init-file :wk "Reload emacs config"))
+;;    (256k/leader-keys
+;;     "h" '(:ignore t :wk "Help")
+;;     "h f" '(describe-function :wk "Describe function")
+;;     "h v" '(describe-variable :wk "Describe variable")
+;;     ;; "h r r" '((lambda () (interactive) (load-file "~/.config/emacs/init.el")) :wk "Reload emacs config"))
+;;     "h r r" '(reload-init-file :wk "Reload emacs config"))
 
-   (256k/leader-keys
-    "t" '(:ignore t :wk "Toggle")
-    "t l" '(display-line-numbers-mode :wk "Toggle line numbers")
-    "t w" '(visual-line-mode :wk "Toggle truncated lines")
-    "t t" '(ansi-term :wk "open ansi-term"))
- )
+;;    (256k/leader-keys
+;;     "t" '(:ignore t :wk "Toggle")
+;;     "t l" '(display-line-numbers-mode :wk "Toggle line numbers")
+;;     "t w" '(visual-line-mode :wk "Toggle truncated lines")
+;;     "t t" '(ansi-term :wk "open ansi-term"))
+;;  )
 
 (defun reload-init-file ()
   (interactive)
   (load-file user-init-file)
   (load-file user-init-file))
+
+;; load custom themes folder
+   (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+;; install doom themes   
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-spacegrey t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;;(doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+  ;; (load-theme 'tfslight t)
 
 (menu-bar-mode 1)
 (tool-bar-mode -1)
@@ -139,6 +166,8 @@
 
 (global-display-line-numbers-mode 1)
 (global-visual-line-mode t)
+
+(add-to-list 'default-frame-alist '(alpha-background . 100)) ; For all new frames henceforth
 
 (use-package all-the-icons
   :ensure t
@@ -154,6 +183,10 @@
 (add-hook 'org-mode-hook 'org-indent-mode)
 (use-package org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(use-package rainbow-mode
+  :diminish
+  :hook org-mode prog-mode)
 
 (use-package which-key
   :init
@@ -219,6 +252,89 @@
   :after company
   :diminish
   :hook (company-mode . company-box-mode))
+
+(use-package dashboard
+  :ensure t 
+  :init
+  (setq initial-buffer-choice 'dashboard-open)
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-banner-logo-title "Electronics. music. coding. art.")
+  ;;(setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
+
+  ;; (setq dashboard-startup-banner "/home/dt/.config/emacs/images/dtmacs-logo.png")  ;; use custom image as banner
+
+  (setq dashboard-center-content nil) ;; set to 't' for centered content
+  (setq dashboard-items '((recents . 5)
+                          (agenda . 5 )
+                          (bookmarks . 3)
+                          (projects . 3)
+                          (registers . 3)))
+  :custom 
+  (dashboard-modify-heading-icons '((recents . "file-text")
+                                      (bookmarks . "book")))
+  :config
+  (dashboard-setup-startup-hook))
+
+(use-package flycheck
+  :ensure t
+  :defer t
+  :diminish
+  :init (global-flycheck-mode))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :config
+  (setq doom-modeline-height 35      ;; sets modeline height
+        doom-modeline-bar-width 5    ;; sets right bar width
+        doom-modeline-persp-name t   ;; adds perspective name to modeline
+        doom-modeline-persp-icon t)) ;; adds folder icon next to persp name
+
+(use-package neotree
+  :config
+  (setq neo-smart-open t
+        neo-show-hidden-files t
+        neo-window-width 55
+        neo-window-fixed-size nil
+        inhibit-compacting-font-caches t
+        projectile-switch-project-action 'neotree-projectile-action) 
+        ;; truncate long file names in neotree
+        (add-hook 'neo-after-create-hook
+           #'(lambda (_)
+               (with-current-buffer (get-buffer neo-buffer-name)
+                 (setq truncate-lines t)
+                 (setq word-wrap nil)
+                 (make-local-variable 'auto-hscroll-mode)
+                 (setq auto-hscroll-mode nil)))))
+
+(use-package projectile
+  :config
+  (projectile-mode 1))
+
+(use-package vterm
+:config
+(setq shell-file-name "/bin/sh"
+      vterm-max-scrollback 5000))
+
+(use-package vterm-toggle
+  :after vterm
+  :config
+  (setq vterm-toggle-fullscreen-p nil)
+  (setq vterm-toggle-scope 'project)
+  (add-to-list 'display-buffer-alist
+               '((lambda (buffer-or-name _)
+                     (let ((buffer (get-buffer buffer-or-name)))
+                       (with-current-buffer buffer
+                         (or (equal major-mode 'vterm-mode)
+                             (string-prefix-p vterm-buffer-name (buffer-name buffer))))))
+                  (display-buffer-reuse-window display-buffer-at-bottom)
+                  ;;(display-buffer-reuse-window display-buffer-in-direction)
+                  ;;display-buffer-in-direction/direction/dedicated is added in emacs27
+                  ;;(direction . bottom)
+                  ;;(dedicated . t) ;dedicated is supported in emacs27
+                  (reusable-frames . visible)
+                  (window-height . 0.4))))
 
 (use-package lua-mode
   :ensure nil
--- init.el
+++ init.el
@@ -1,6 +1,12 @@
 (toggle-frame-maximized)
 
+
 (org-babel-load-file
  (expand-file-name
   "config.org"
+  user-emacs-directory))
+
+(org-babel-load-file
+ (expand-file-name
+  "bindings.org"
   user-emacs-directory))
