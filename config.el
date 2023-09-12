7cdae7d35718af16d67f02e0db8f2fd70974d941
256k <fayek.helmi@gmail.com>
2023-09-12 17:37:33 -0400

    removed some packages and removed evil mode

--- config.el
+++ config.el
@@ -60,21 +60,22 @@
 ;;(elpaca-wait)
 
 ;; Expands to: (elpaca evil (use-package evil :demand t))
-(use-package evil
-    :init      ;; tweak evil's configuration before loading it
-    (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-    (setq evil-want-keybinding nil)
-    (setq evil-vsplit-window-right t)
-    (setq evil-split-window-below t)
-    (evil-mode))
-  (use-package evil-collection
-    :after evil
-    :config
-    (setq evil-collection-mode-list '(dashboard dired ibuffer))
-    (evil-collection-init))
-  (use-package evil-tutor)
+;; (use-package evil
+;;     :init      ;; tweak evil's configuration before loading it
+;;     (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+;;     (setq evil-want-keybinding nil)
+;;     (setq evil-vsplit-window-right t)
+;;     (setq evil-split-window-below t)
+;;     (evil-mode))
+;;   (use-package evil-collection
+;;     :after evil
+;;     :config
+;;     (setq evil-collection-mode-list '(dashboard dired ibuffer))
+;;     (evil-collection-init))
+;;   (use-package evil-tutor)
 
-;;Turns off elpaca-use-package-mode current declartion
+;; ;;Turns off elpaca-use-pa
+;;  ckage-mode current declartion
 ;;Note this will cause the declaration to be interpreted immediately (not deferred).
 ;;Useful for configuring built-in emacs features.
 (use-package emacs :elpaca nil :config (setq ring-bell-function #'ignore))
@@ -82,90 +83,46 @@
 ;; Don't install anything. Defer execution of BODY
 ;; (elpaca nil (message "deferred"))
 
-;; (use-package general
-;;   :config
-;;   (general-evil-setup)
-
-;;   ;; set up 'SPC' as the global leader key
-;;   (general-create-definer 256k/leader-keys
-;;     :states '(normal insert visual emacs)
-;;     :keymaps 'override
-;;     :prefix "SPC" ;; set leader
-;;     :global-prefix "M-SPC") ;; access leader in insert mode
-
-;;   (256k/leader-keys
-;;     "." '(find-file :wk "Find file")
-;;     "TAB TAB" '(comment-line :wk "Comment lines"))
-
-;;   (256k/leader-keys
-;;     "b" '(:ignore t :wk "buffer")
-;;     "b b" '(switch-to-buffer :wk "Switch buffer")
-;;     "b i" '(ibuffer :wk "Ibuffer")
-;;     "b k" '(kill-this-buffer :wk "Kill this buffer")
-;;     "b n" '(next-buffer :wk "Next buffer")
-;;     "b p" '(previous-buffer :wk "Previous buffer")
-;;     "b r" '(revert-buffer :wk "Reload buffer"))
-
-;;   (256k/leader-keys
-;;     "f"   '(:ignore t :wk "terminal")
-;;     "f f" '((lambda () (interactive) (find-file "./")) :wk "open file browser")
-;;     "f c" '((lambda () (interactive) (find-file "~/.emacs.d/config.org")) :wk "Edit emacs config")
-;;     "f r"   '(counsel-recentf :wk "toggle file details")
-;;     "d"   '(dired-hide-details-mode :wk "toggle file details"))
-
-;;   (256k/leader-keys
-;;     "e" '(:ignore t :wk "Evaluate")    
-;;     "e b" '(eval-buffer :wk "Evaluate elisp in buffer")
-;;     "e d" '(eval-defun :wk "Evaluate defun containing or after point")
-;;     "e e" '(eval-expression :wk "Evaluate and elisp expression")
-;;     "e l" '(eval-last-sexp :wk "Evaluate elisp expression before point")
-;;     "e r" '(eval-region :wk "Evaluate elisp in region")) 
-
-;;    (256k/leader-keys
-;;     "h" '(:ignore t :wk "Help")
-;;     "h f" '(describe-function :wk "Describe function")
-;;     "h v" '(describe-variable :wk "Describe variable")
-;;     ;; "h r r" '((lambda () (interactive) (load-file "~/.config/emacs/init.el")) :wk "Reload emacs config"))
-;;     "h r r" '(reload-init-file :wk "Reload emacs config"))
-
-;;    (256k/leader-keys
-;;     "t" '(:ignore t :wk "Toggle")
-;;     "t l" '(display-line-numbers-mode :wk "Toggle line numbers")
-;;     "t w" '(visual-line-mode :wk "Toggle truncated lines")
-;;     "t t" '(ansi-term :wk "open ansi-term"))
-;;  )
-
-(defun reload-init-file ()
-  (interactive)
-  (load-file user-init-file)
-  (load-file user-init-file))
-
 ;; load custom themes folder
-         (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-      ;; install doom themes   
-      (use-package doom-themes
-        :ensure t
-        :config
-        ;; Global settings (defaults)
-        (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-              doom-themes-enable-italic t) ; if nil, italics is universally disabled
-        ;; (load-theme 'doom-spacegrey t)
+                  ;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+               ;; install doom themes   
+       ;; (use-package doom-themes
+       ;; 	  :ensure t
+       ;; 	  :config
+       ;; 	  ;; Global settings (defaults)
+       ;; 	  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+       ;; 		doom-themes-enable-italic t) ; if nil, italics is universally disabled
+       ;; 	  ;; (load-theme 'doom-spacegrey t)
 
-        ;; Enable flashing mode-line on errors
-        (doom-themes-visual-bell-config)
-        ;; Enable custom neotree theme (all-the-icons must be installed!)
-        (doom-themes-neotree-config)
-        ;;(doom-themes-treemacs-config)
-        ;; Corrects (and improves) org-mode's native fontification.
-        (doom-themes-org-config))
+       ;; 	  ;; Enable flashing mode-line on errors
+       ;; 	  (doom-themes-visual-bell-config)
+       ;; 	  ;; Enable custom neotree theme (all-the-icons must be installed!)
+       ;; 	  (doom-themes-neotree-config)
+       ;; 	  ;;(doom-themes-treemacs-config)
+       ;; 	  ;; Corrects (and improves) org-mode's native fontification.
+       ;; 	  (doom-themes-org-config))
 
+(use-package catppuccin-theme
+  :ensure t
+  :config
+  ;; 'frappe, 'latte, 'macchiato, or 'mocha
+  (setq catppuccin-flavor 'frappe)
+  (load-theme 'catppuccin t))
 
-      (use-package catppuccin-theme
-:ensure t
-:config
-;; 'frappe, 'latte, 'macchiato, or 'mocha
-      (setq catppuccin-flavor 'frappe)
-      (load-theme 'catppuccin t))
+ ;; kaolin-dark - a dark jade variant inspired by Sierra.vim
+ ;; kaolin-light - light variant of the original kaolin-dark.
+ ;; kaolin-aurora - Kaolin meets polar lights.
+ ;; kaolin-bubblegum - Kaolin colorful theme with dark blue background.
+ ;; kaolin-eclipse - a dark purple variant
+ ;; kaolin-galaxy - bright theme based on one of the Sebastian Andaur arts.
+ ;; kaolin-ocean - a dark blue variant.
+ ;; kaolin-temple - dark background with syntax highlighting focus on blue, green and pink shades
+ ;; kaolin-valley-dark - colorful Kaolin theme with brown background.
+ ;; kaolin-valley-light - light variant of kaolin-valley theme.
+
+;; (use-package kaolin-themes
+;;   :config
+;;   (load-theme 'kaolin-dark t))
 
 (menu-bar-mode 1)
 (tool-bar-mode -1)
@@ -258,64 +215,11 @@
   :diminish
   :hook (company-mode . company-box-mode))
 
-(use-package dashboard
-  :ensure t 
-  :init
-  (setq initial-buffer-choice 'dashboard-open)
-  (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t)
-  (setq dashboard-banner-logo-title "Electronics. music. coding. art.")
-  ;;(setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
-
-  ;; (setq dashboard-startup-banner "/home/dt/.config/emacs/images/dtmacs-logo.png")  ;; use custom image as banner
-
-  (setq dashboard-center-content nil) ;; set to 't' for centered content
-  (setq dashboard-items '((recents . 5)
-                          (agenda . 5 )
-                          (bookmarks . 3)
-                          (projects . 3)
-                          (registers . 3)))
-  :custom 
-  (dashboard-modify-heading-icons '((recents . "file-text")
-                                      (bookmarks . "book")))
-  :config
-  (dashboard-setup-startup-hook))
-
 (use-package flycheck
   :ensure t
   :defer t
   :diminish
   :init (global-flycheck-mode))
-
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :config
-  (setq doom-modeline-height 20      ;; sets modeline height
-        doom-modeline-bar-width 5    ;; sets right bar width
-        doom-modeline-persp-name t   ;; adds perspective name to modeline
-        doom-modeline-persp-icon t)) ;; adds folder icon next to persp name
-
-(use-package neotree
-  :config
-  (setq neo-smart-open t
-        neo-show-hidden-files t
-        neo-window-width 55
-        neo-window-fixed-size nil
-        inhibit-compacting-font-caches t
-        projectile-switch-project-action 'neotree-projectile-action) 
-        ;; truncate long file names in neotree
-        (add-hook 'neo-after-create-hook
-           #'(lambda (_)
-               (with-current-buffer (get-buffer neo-buffer-name)
-                 (setq truncate-lines t)
-                 (setq word-wrap nil)
-                 (make-local-variable 'auto-hscroll-mode)
-                 (setq auto-hscroll-mode nil)))))
-
-(use-package projectile
-  :config
-  (projectile-mode 1))
 
 (use-package vterm
 :config
