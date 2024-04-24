0619cdba599b71b70e085853c27f0758e24fe5a2
fayek helmi <fayek.helmi@gmail.com>
2024-04-24 09:43:46 -0400

    updated theme and shortcut for font toggle

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(solo-jazz-theme which-key vertico try treesit-auto slime org-bullets norns marginalia magit lsp-ui git-gutter forth-mode flycheck-eglot expand-region exec-path-from-shell evil doom-themes consult-lsp company acme-theme)))
+   '(atom-one-dark-theme solo-jazz-theme which-key vertico try treesit-auto slime org-bullets norns marginalia magit lsp-ui git-gutter forth-mode flycheck-eglot expand-region exec-path-from-shell evil doom-themes consult-lsp company acme-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
